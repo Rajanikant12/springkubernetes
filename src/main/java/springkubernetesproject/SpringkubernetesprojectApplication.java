e1bd2478e5c4bbf41d6de6ce4e65a248f8ae1187
iamrky3 <rkgecrajni321@gmail.com>
2024-10-13 14:46:28 +0530

    initial commit

---
+++ src/main/java/springkubernetesproject/SpringkubernetesprojectApplication.java
@@ -0,0 +1,13 @@
+package springkubernetesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringkubernetesprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringkubernetesprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springkubernetesproject/SpringkubernetesprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package springkubernetesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringkubernetesprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
