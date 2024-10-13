f18a5f0e221aa12eee2a5519ced38ecf1128a9fd
iamrky3 <rkgecrajni321@gmail.com>
2024-10-13 15:47:57 +0530

    docker

--- src/main/java/springkubernetesproject/SpringkubernetesprojectApplication.java
+++ src/main/java/springkubernetesproject/SpringkubernetesprojectApplication.java
@@ -3,14 +3,28 @@ package springkubernetesproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@RestController
 public class SpringkubernetesprojectApplication {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Friends";
+	}
+
 
 	public static void main(String[] args) {
+		
+	
+		
 		SpringApplication.run(SpringkubernetesprojectApplication.class, args);
 		System.out.println("Hello Kubernetes and Docker");
 		System.out.println("I am learning DevOps and cloud");
+		
 		
 	}
 
