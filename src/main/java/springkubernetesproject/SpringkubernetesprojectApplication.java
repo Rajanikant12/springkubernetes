e48ec311a1a37074aa6a911b282e1f4df125874a
iamrky3 <rkgecrajni321@gmail.com>
2024-10-13 14:57:41 +0530

    hello change

--- src/main/java/springkubernetesproject/SpringkubernetesprojectApplication.java
+++ src/main/java/springkubernetesproject/SpringkubernetesprojectApplication.java
@@ -2,12 +2,15 @@ package springkubernetesproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SpringkubernetesprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringkubernetesprojectApplication.class, args);
+		System.out.println("Hello Kubernetes");
+		
 	}
 
 }
